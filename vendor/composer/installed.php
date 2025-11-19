07dba8555cf5caba735b35cb11c62780053f6e93
Fujii <167102935+Fujiiww@users.noreply.github.com>
2025-11-18 22:14:07 -0300

    add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'carli/sistema-de-controle-de-estacionamento',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c8aabd079f65b9528267c4d49f54b57b54e15048',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'carli/sistema-de-controle-de-estacionamento' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c8aabd079f65b9528267c4d49f54b57b54e15048',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
